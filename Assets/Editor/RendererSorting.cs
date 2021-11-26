f39a406dc92aeacb875496d337eea2f46a6aabc7
unknown <yh821@qq.com>
2021-11-26 15:38:05 +0800

    添加场景检查

---
+++ Assets/Editor/RendererSorting.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MeshRenderer))]
+public class MeshRendererSorting : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var renderer = target as MeshRenderer;
+
+        var layerNames = new string[SortingLayer.layers.Length];
+        for(int i = 0; i < layerNames.Length; i++)
+        {
+            layerNames[i] = SortingLayer.layers[i].name;
+        }
+
+        var layerValue = SortingLayer.GetLayerValueFromID(renderer.sortingLayerID);
+        layerValue = EditorGUILayout.Popup("Sorting Layer", layerValue, layerNames);
+
+        var layer = SortingLayer.layers[layerValue];
+        renderer.sortingLayerName = layer.name;
+        renderer.sortingLayerID = layer.id;
+        renderer.sortingOrder = EditorGUILayout.IntField("Order in Layer", renderer.sortingOrder);
+
+        foreach(var mat in renderer.materials)
+        {
+            mat.renderQueue = EditorGUILayout.IntField(mat.name, mat.renderQueue);
+        }
+    }
+}
+
+[CustomEditor(typeof(SkinnedMeshRenderer))]
+public class SkinnedMeshRendererSorting : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var renderer = target as SkinnedMeshRenderer;
+
+        var layerNames = new string[SortingLayer.layers.Length];
+        for (int i = 0; i < layerNames.Length; i++)
+        {
+            layerNames[i] = SortingLayer.layers[i].name;
+        }
+
+        var layerValue = SortingLayer.GetLayerValueFromID(renderer.sortingLayerID);
+        layerValue = EditorGUILayout.Popup("Sorting Layer", layerValue, layerNames);
+
+        var layer = SortingLayer.layers[layerValue];
+        renderer.sortingLayerName = layer.name;
+        renderer.sortingLayerID = layer.id;
+        renderer.sortingOrder = EditorGUILayout.IntField("Order in Layer", renderer.sortingOrder);
+
+        foreach (var mat in renderer.materials)
+        {
+            mat.renderQueue = EditorGUILayout.IntField(mat.name, mat.renderQueue);
+        }
+    }
+}
--- Assets/Editor/Toolbar/UnityTopBar.cs
+++ Assets/Editor/Toolbar/UnityTopBar.cs
@@ -1,20 +1,58 @@
+using System;
+using System.Collections.Generic;
 using Common;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 using UnityToolbarExtender;
 
 [InitializeOnLoad]
 public class UnityTopBar
 {
+    public const string ProjName = "S1:uc03_cn";
+
+    private static GUIStyle _style;
+    private static bool _needCheckScene = false;
+
     static UnityTopBar()
     {
         ToolbarExtender.LeftToolbarGUI.Add(OnLeftToolbarGUI);
         ToolbarExtender.RightToolbarGUI.Add(OnRightToolbarGUI);
+        EditorSceneManager.sceneOpened += OnSceneOpened;
+        EditorSceneManager.newSceneCreated += OnSceneCreated;
+    }
+
+    static void OnSceneOpened(Scene scene, OpenSceneMode mode)
+    {
+        _needCheckScene = scene.path.StartsWith("Assets/Game/Scenes/Map/");
+        if (_needCheckScene)
+        {
+            var roots = scene.GetRootGameObjects();
+            foreach(var go in roots)
+            {
+                if(go.name.ToLower() == "main")
+                {
+                    return;
+                }
+            }
+            _needCheckScene = false;
+        }
+    }
+
+    static void OnSceneCreated(Scene scene, NewSceneSetup setup, NewSceneMode mode)
+    {
+        _needCheckScene = false;
     }
 
     static void OnLeftToolbarGUI()
     {
+        if (_style == null)
+        {
+            _style = new GUIStyle("BoldLabel") { fontSize = 16 };
+        }
+
         if (GUILayout.Button(new GUIContent("提交", "提交unity工程svn"), GUILayout.Height(22)))
         {
             SVNHelper.CommitToSVN();
@@ -30,6 +68,8 @@ public class UnityTopBar
         GUI.color = Color.white;
 
         GUILayout.FlexibleSpace();
+
+        GUILayout.Label(ProjName, _style);
     }
 
     static void OnRightToolbarGUI()
@@ -40,6 +80,8 @@ public class UnityTopBar
         if (GUILayout.Button(new GUIContent("空场景", "新建空场景"), GUILayout.Height(22)))
         {
             EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
+            var camera = new GameObject("Camera", typeof(Camera));
+            camera.transform.position = new Vector3(0, 1, -10);
         }
 
         GUI.color = Color.green;
@@ -47,8 +89,74 @@ public class UnityTopBar
         {
             EditorSceneManager.OpenScene("Assets/Scenes/main.unity");
         }
-        GUI.color = Color.white;
 
+        if (_needCheckScene)
+        {
+            GUI.color = Color.red;
+            if (GUILayout.Button(new GUIContent("检查场景", "检查场景"), GUILayout.Height(22)))
+            {
+                SceneCheckList();
+            }
+        }
+
+            GUI.color = Color.white;
         GUILayout.FlexibleSpace();
+    }
+
+    private static void SceneCheckList()
+    {
+        var content = new System.Text.StringBuilder();
+        var scene = EditorSceneManager.GetActiveScene();
+
+        var gameObjects = new List<GameObject>();
+        var roots = scene.GetRootGameObjects();
+        foreach(var go in roots)
+        {
+            var children = go.GetComponentsInChildren<Transform>(true);
+            foreach(var child in children)
+            {
+                gameObjects.Add(child.gameObject);
+            }
+        }
+
+        foreach(var go in gameObjects)
+        {
+            Debug.Log(go.name);
+        }
+
+
+        //是否烘培
+        if(Lightmapping.lightingDataAsset == null)
+        {
+            content.Append("缺少光照贴图, 检查是否已烘培!");
+        }
+
+        //寻路网格
+        var triangulation = NavMesh.CalculateTriangulation();
+        if(triangulation.vertices.Length <= 0)
+        {
+            content.Append("缺少寻路网格, 检查是否已烘培!");
+        }
+
+        //环境光
+        var lightColor = RenderSettings.ambientLight;
+        var grayLevel = lightColor.r * 0.299f + lightColor.g * 0.587f + lightColor.b * 0.114f;
+        if (grayLevel < 0.75f)
+        {
+            RenderSettings.ambientLight = Color.white;
+            content.Append("已将环境光改为白色!");
+        }
+
+        if (content.Length > 0)
+        {
+            Debug.LogError(content.ToString());
+            EditorUtility.DisplayDialog("错误", content.ToString(), "确定");
+        }
+        else
+        {
+            Debug.Log("场景检查完成");
+        }
+
+        EditorSceneManager.SaveScene(scene);
     }
 }
