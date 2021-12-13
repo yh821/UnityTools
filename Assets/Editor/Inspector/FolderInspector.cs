2a841500afb311d21e0a29504f7dd74c18c0fb94
unknown <yh821@qq.com>
2021-12-13 15:21:27 +0800

    文件夹Inspector界面添加GUID转换

---
+++ Assets/Editor/Inspector/FolderInspector.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UObject = UnityEngine.Object;
+
+[CustomEditor(typeof(DefaultAsset))]
+public class FolderInspector : Editor
+{
+    private string mGuid;
+    private string mLastGuid;
+    private UObject mObj;
+    private UObject mLastObj;
+    private string mPath;
+    private string mLastPath;
+
+    public override void OnInspectorGUI()
+    {
+        var defaultLabelWidth = EditorGUIUtility.labelWidth;
+        EditorGUIUtility.labelWidth = 64;
+        GUI.enabled = true;
+        {
+            if (mObj == null)
+                mObj = target;
+
+            if (mGuid != mLastGuid)
+            {
+                mLastGuid = mGuid;
+                mPath = AssetDatabase.GUIDToAssetPath(mGuid);
+                mObj = AssetDatabase.LoadAssetAtPath<UObject>(mPath);
+                mLastObj = mObj;
+                mLastPath = mPath;
+            }
+
+            if (mObj != mLastObj)
+            {
+                mLastObj = mObj;
+                mPath = AssetDatabase.GetAssetPath(mObj);
+                mGuid = AssetDatabase.AssetPathToGUID(mPath);
+                mLastGuid = mGuid;
+                mLastPath = mPath;
+            }
+
+            if (mPath != mLastPath)
+            {
+                mLastPath = mPath;
+                mObj = AssetDatabase.LoadAssetAtPath<UObject>(mPath);
+                mGuid = AssetDatabase.AssetPathToGUID(mPath);
+                mLastObj = mObj;
+                mLastGuid = mGuid;
+            }
+
+            mGuid = EditorGUILayout.TextField("GUID:", mGuid);
+            mPath = EditorGUILayout.TextField("Path:", mPath);
+            mObj = EditorGUILayout.ObjectField("Asset:", mObj, typeof(UObject), false);
+        }
+        EditorGUIUtility.labelWidth = defaultLabelWidth;
+        GUI.enabled = false;
+        base.OnInspectorGUI();
+    }
+}
