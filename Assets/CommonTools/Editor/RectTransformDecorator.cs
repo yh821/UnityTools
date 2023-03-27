bd3bf3f9a1d53e57e8fb70f0afa2916459aafaa7
hugo <hugo6@qq.com>
2023-03-27 21:23:14 +0800

    添加Transform属性面板按钮

---
+++ Assets/CommonTools/Editor/RectTransformDecorator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RectTransform), true)]
+public class RectTransformDecorator : DecoratorEditor
+{
+	public RectTransformDecorator() : base("RectTransformInspector")
+	{
+	}
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		if (GUILayout.Button("取整"))
+		{
+			var rt = serializedObject.targetObject as RectTransform;
+			var pos = rt.anchoredPosition3D;
+			rt.anchoredPosition3D =
+				new Vector3(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y), Mathf.RoundToInt(pos.z));
+			var size = rt.sizeDelta;
+			rt.sizeDelta = new Vector2(Mathf.RoundToInt(size.x), Mathf.RoundToInt(size.y));
+		}
+	}
+}
---
+++ Assets/CommonTools/Editor/TransformDecorator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Transform), true)]
+public class TransformDecorator : DecoratorEditor
+{
+	public TransformDecorator() : base("TransformInspector")
+	{
+	}
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		if (GUILayout.Button("取整"))
+		{
+			var trans = serializedObject.targetObject as Transform;
+			var pos = trans.localPosition;
+			trans.localPosition =
+				new Vector3(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y), Mathf.RoundToInt(pos.z));
+		}
+	}
+}
